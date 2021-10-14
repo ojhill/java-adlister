cf6924cc8150bab43be9fbe2d482e38fc8d98205
Orenthal Hill <ojhill98@gmail.com>
2021-10-13 20:54:17 -0500

    count exercise complete

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "Count", urlPatterns = {"/count"})
+public class CountServlet extends HttpServlet {
+
+    int count = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        //   BONUS: Allow the user to pass a parameter in the query string to reset the counter.
+        String passedCount = request.getParameter("count");
+        // if there is no name in the url query, it will return null, so then we set null equal to a string
+        if (passedCount != null) count = Integer.valueOf(passedCount);
+            //every time user requests the page we increment a count plus one
+        else count ++;
+        PrintWriter out = response.getWriter();
+        out.println("<h1> Page Viewed: " + count + " time(s).</h1>");
+    }
+
+}
+
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,22 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+//        out.println("<h1>Hello, world!</h1>");
+        String name = req.getParameter("name");
+        if(name != null){
+        out.println("<h1>Hello," + name + "!</h1>");
+        }else{
+            out.println("<h1>Hello,world</h1>");
+        }
     }
 }
